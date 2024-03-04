49e187a91384e6ab18876631cae5429e794051b4
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-04 15:58:24 -0300

    created word length game :D

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,34 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
-
+    @letters = session[:letters] = generate_grid(10)
+    session[:score] = 0 if session[:score].nil?
   end
+
+  def score
+    @word = params[:word]
+    @letters = session[:letters]
+    @word_in_grid = word_in_grid?(@word, @letters)
+    @found = found? @word
+    (session[:score] += @word.length) if @word_in_grid && @found
+    @score = session[:score]
+  end
+end
+
+def generate_grid(grid_size)
+  grid_size.times.map { ('A'..'Z').to_a.sample }
+end
+
+def word_in_grid?(word, grid)
+  word.upcase.chars.none? do |letter|
+    grid.delete_at(grid.index(letter) || grid.length).nil? # clever!
+  end
+end
+
+def found?(word)
+  api_url = "https://wagon-dictionary.herokuapp.com/#{word}"
+  word_info = JSON.parse URI.open(api_url).read
+  word_info['found']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 #create route,controller, action and view for new
 # create route,controller, action and view for score
 
-
 Rails.application.routes.draw do
   get 'new', to: 'games#new'
+  post 'score', to: 'games#score'
 end
